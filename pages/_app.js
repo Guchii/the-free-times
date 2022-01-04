eb709b1474f928510267d22c2976f7bf4d9c7341
Guchii <shvomsrivastava@gmail.com>
2022-01-04 14:26:48 +0530

    🚸 category based fetching

--- pages/_app.js
+++ pages/_app.js
@@ -1,47 +1,31 @@
 import Head from "next/head";
 import styles from "../styles/home.module.css";
+import GithubCorner from "react-github-corner";
 import "../styles/globals.css";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const [category, setCategory] = useState("technology");
   return (
     <div>
       {/* header */}
       <header className={styles.header}>
         <h1>THE FREE TIMES</h1>
         <p>
-          <i>
-            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-            consectetur, adipisci velit...
-          </i>
-        </p>
-        <p>
-          There is no one who loves pain itself, who seeks after it and wants to
-          have it, simply because it is pain..
+          <i>Free to read e-paper with concise articles and many categories</i>
         </p>
         <ul className={styles.linkBox}>
-          <li>
-            <a href="">technology</a>
-          </li>
-          <li>
-            <a href="">national</a>
-          </li>
-          <li>
-            <a href="">business</a>
-          </li>
-          <li>
-            <a href="">sports</a>
-          </li>
-          <li>
-            <a href="">world</a>
-          </li>
-          <li>
-            <a href="">politics</a>
-          </li>
+          <li onClick={() => setCategory("technology")}>technology</li>
+          <li onClick={() => setCategory("national")}>national</li>
+          <li onClick={() => setCategory("business")}>business</li>
+          <li onClick={() => setCategory("sports")}>sports</li>
+          <li onClick={() => setCategory("world")}>world</li>
+          <li onClick={() => setCategory("politics")}>politics</li>
         </ul>
         <hr />
       </header>
       <main>
-        <Component {...pageProps} />
+        <Component {...pageProps} {...{ category, setCategory }} />
       </main>
       <footer className={styles.footer}>
         <p>
@@ -49,6 +33,11 @@ function MyApp({ Component, pageProps }) {
           Please star.
         </p>
       </footer>
+      <GithubCorner
+        href="https://github.com/Guchii/the-free-times"
+        octoColor="#f3dfc1"
+        bannerColor="#1a1c1a"
+      />
       <Head>
         <title>
           Free to read e-paper. Stay updated with the latest news. The free
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,54 @@
+/* eslint-disable @next/next/no-img-element */
 import axios from "axios";
 import { v4 } from "uuid";
 import styles from "../styles/index.module.css";
+import { useEffect, useState } from "react";
 
-export default function Home({ data }) {
-  return (
-    <>
-      <div className={`${styles.mainGrid} ${styles.maxWidth}`}>
-        {data.map((dat) => (
-          <div className={styles.article} key={v4()}>
-            <h2>{dat.title}</h2>
-            <p>{dat.content}</p>
-            <a href={dat.readMoreUrl} target="_blank" rel="noreferrer">
-              Read more...
-            </a>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+const Loading = () => (
+  <div
+    style={{
+      margin: "10rem auto",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <img src="/rocket.gif" alt="loading" />
+    loading the paper
+  </div>
+);
+
+export default function Home({ category: category }) {
+  const [arr, setArr] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async (category) => {
+      setLoading(true);
+      const {
+        data: { data },
+      } = await axios.get(
+        `https://inshortsapi.vercel.app/news?category=${category}`
+      );
+      setArr(data);
+      setLoading(false);
+    })(category);
+  }, [category]);
+  return loading ? <Loading /> : <Grid data={arr} />;
 }
 
-export const getStaticProps = async () => {
-  const {
-    data: { data },
-  } = await axios.get(
-    "https://inshortsapi.vercel.app/news?category=technology"
+const Grid = ({ data }) => {
+  return (
+    <div className={`${styles.mainGrid} ${styles.maxWidth}`}>
+      {data.map((dat) => (
+        <div className={styles.article} key={v4()}>
+          <h2>{dat.title}</h2>
+          <p>{dat.content}</p>
+          <a href={dat.readMoreUrl} target="_blank" rel="noreferrer">
+            Read more...
+          </a>
+        </div>
+      ))}
+    </div>
   );
-  return {
-    props: {
-      data,
-    },
-    revalidate: 60,
-  };
 };
