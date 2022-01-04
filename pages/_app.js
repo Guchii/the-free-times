05943080cbc2ddeb495bbfb3b9f15a38a2abae73
Guchii <shvomsrivastava@gmail.com>
2022-01-04 12:53:16 +0530

    🎉 good start

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,62 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import styles from "../styles/home.module.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      {/* header */}
+      <header className={styles.header}>
+        <h1>THE FREE TIMES</h1>
+        <p>
+          <i>
+            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit...
+          </i>
+        </p>
+        <p>
+          There is no one who loves pain itself, who seeks after it and wants to
+          have it, simply because it is pain..
+        </p>
+        <ul className={styles.linkBox}>
+          <li>
+            <a href="">technology</a>
+          </li>
+          <li>
+            <a href="">national</a>
+          </li>
+          <li>
+            <a href="">business</a>
+          </li>
+          <li>
+            <a href="">sports</a>
+          </li>
+          <li>
+            <a href="">world</a>
+          </li>
+          <li>
+            <a href="">politics</a>
+          </li>
+        </ul>
+        <hr />
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer className={styles.footer}>
+        <p>
+          This is a free app and under active development on <b>GitHub</b>.
+          Please star.
+        </p>
+      </footer>
+      <Head>
+        <title>
+          Free to read e-paper. Stay updated with the latest news. The free
+          times
+        </title>
+      </Head>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Neuton:ital,wght@0,400;0,700;1,400&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📰</text></svg>"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
