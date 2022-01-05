88201af77aaf1b4d6894fb0af76d913950693e4b
Guchii <71770229+Guchii@users.noreply.github.com>
2022-01-05 15:14:13 +0000

    Deploying to gh-pages from @ Guchii/the-free-times@6ffd4154213c4ec4d97155c22e22c70838edf8d1 🚀

---
+++ _next/static/9xqUxHi4_QqwvV904OKPJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/922-704e86327b8a6319.js","static/css/cf51837789a0ed8c.css","static/chunks/pages/index-fc4c02660192cbc7.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
